c8f3e3dcdea010e0709e47df78868c678a4d1af2
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2018-06-21 16:50:04 +0700

    Add wifi, RGB LED

--- ogopark/ogopark.ino
+++ ogopark/ogopark.ino
@@ -1,13 +1,94 @@
+/*
+ * 
+ * 
+
+MIT License
+
+Copyright (c) 2018 Pornthep Nivatyakul
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*
+*
+*/
+
+#include <Adafruit_NeoPixel.h>
+
+#include <ESP8266WiFi.h>
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
 #define trigPin D7
 #define echoPin D6
+#define TRIGGER_PIN D3
+#define PIN D2
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (115200);
+  delay(10);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(D4, OUTPUT);
+  pinMode(TRIGGER_PIN, INPUT);
 
+  pixels.begin(); // This initializes the NeoPixel library.
+  pixels.setBrightness(64);
+
+  pixels.setPixelColor(0, 255, 255, 0); // yellow #FFFF00
+  pixels.show();
+  
+  Serial.println();
+  Serial.println();
+  Serial.println(WiFi.SSID());
+  Serial.println(WiFi.psk());
+  String SSID = WiFi.SSID();
+  String PSK = WiFi.psk();
+  WiFi.begin();
+  Serial.print("Connecting");
+  Serial.println();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if ( digitalRead(TRIGGER_PIN) == LOW ) {
+      ondemandWiFi();
+    }
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP()); 
+  pixels.setPixelColor(0, 0, 0, 255); // blue #0000FF
+  pixels.show();
+  delay(1000);
+  pixels.setPixelColor(0, 0, 0, 0);
+  pixels.show();
+  delay(500);
+  pixels.setPixelColor(0, 0, 0, 255); // blue #0000FF
+  pixels.show();
+  delay(1000);
+  pixels.setPixelColor(0, 0, 0, 0);
+  pixels.show();
 }
 
 void loop() {
@@ -21,11 +102,15 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-  if (distance < 4) { 
+  if (distance < 15) { 
     digitalWrite(D4, LOW);
+    pixels.setPixelColor(0, 255, 0, 0); // red #FF0000
+    pixels.show();
   }
   else {
     digitalWrite(D4, HIGH);
+    pixels.setPixelColor(0, 0, 128, 0); // green #008000
+    pixels.show();
   }
   if (distance >= 200 || distance <= 0){
     Serial.println("Out of range");
@@ -35,4 +120,31 @@ void loop() {
     Serial.println(" cm");
   }
   delay(500);
+
+    // put your main code here, to run repeatedly:
+  if ( digitalRead(TRIGGER_PIN) == LOW ) {
+    ondemandWiFi();
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if ( digitalRead(TRIGGER_PIN) == LOW ) {
+      ondemandWiFi();
+    }
+  }
+}
+
+void ondemandWiFi()
+{
+    WiFiManager wifiManager;
+    if (!wifiManager.startConfigPortal("OnDemandAP")) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.reset();
+      delay(5000);
+    }
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
 }
